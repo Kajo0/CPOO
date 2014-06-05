162f8366289cbc5971d52f163c1f4f0e41188d83
asitek <albert.sitek@gmail.com>
2014-06-05 23:42:45 +0200

    Drago algorithm implemented

--- src/main/java/pl/edu/pw/elka/cpoo/algorithms/ToneMappingAlg2.java
+++ src/main/java/pl/edu/pw/elka/cpoo/algorithms/ToneMappingAlg2.java
@@ -2,12 +2,10 @@ package pl.edu.pw.elka.cpoo.algorithms;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import pl.edu.pw.elka.cpoo.ArrayTools;
 import pl.edu.pw.elka.cpoo.images.HdrImage;
 import pl.edu.pw.elka.cpoo.images.ImageWrapper;
 import pl.edu.pw.elka.cpoo.images.MyImage;
@@ -15,8 +13,10 @@ import pl.edu.pw.elka.cpoo.interfaces.HdrProcessor;
 
 public class ToneMappingAlg2 implements HdrProcessor {
 
+	private static final double[] RGB2XYZ = { 0.5141364, 0.3238786, 0.16036376, 0.265068, 0.67023428, 0.06409157, 0.0241188, 0.1228178, 0.84442666 };
+    private static final double[] XYZ2RGB = { 2.5651, -1.1665, -0.3986, -1.0217, 1.9777, 0.0439, 0.0753, -0.2543, 1.1892 };
+      
     private HdrImage hdrImage;
-    private double bias;
 
     @Override
     public Image process(final ImageWrapper imageWrapper) {
@@ -27,8 +27,6 @@ public class ToneMappingAlg2 implements HdrProcessor {
 
         hdrImage = new HdrImage(images);
         hdrImage.process();
-
-        hdrImage.showRawTool();
 
         return doTonalMappingAlg2(hdrImage);
     }
@@ -43,104 +41,99 @@ public class ToneMappingAlg2 implements HdrProcessor {
         return "Tonal Mapping 2";
     }
     
-    private float[] calculateLuminance(int size, float[] Y) {
-        float avLum = 0.0f;
-        float maxLum = 0.0f;
-
-        for (int i = 0; i < size; i++) {
-            avLum += Math.log(Y[i] + 1e-4);
-            maxLum = (Y[i] > maxLum) ? Y[i] : maxLum;
-        }
-        avLum = (float) Math.exp(avLum / size);
-        return new float[]{avLum, maxLum};
-    }
-
-    public double biasFunc(double b, double x) {
-        return Math.pow(x, b);          // pow(x, log(bias)/log(0.5)
-    }
-    
-    /**
-     * Drago tonal mapping alghoritm
-     * 
-     * @param hdrImage
-     * 
-     * @return LDR image
-     */
     private Image doTonalMappingAlg2(HdrImage hdrImage) {
 
-        float[] pixels = hdrImage.getHdrData();
+        // dane hdr
+        float[] rgb = hdrImage.getHdrData();
+        int size = (rgb.length / 3);
         
-        int length = hdrImage.getWidth() * hdrImage.getHeight();
-        double[][] out = new double[hdrImage.getWidth()][hdrImage.getHeight()];
-        float[] luminance = calculateLuminance(length, pixels);
-        double maxLum = luminance[1];
-        double avLum = luminance[0];
-        maxLum /= avLum;
-        float[][] pixels2D = ArrayTools.convert1D2D(pixels, hdrImage.getWidth(), hdrImage.getHeight());
-        System.out.println(pixels2D.length);
-        System.out.println(pixels2D[1].length);
-        double LOG05 = -0.693147; // log(0.5)
-        float biasP = (float) (Math.log(bias) / LOG05);
+        double max = 0;
+        double avg = 0;
+        
+        double maxLuminance;
+        double avgLuminance;
+        double divider;
 
-        double divider = Math.log10(maxLum + 1.0);
-        //System.out.println(pixels2D[126][128]);
+        // konwersja obrazu hdr do Yxy i wyliczenie maksymalnej, sredniej oraz
+        // œredniej logaryticznej wartoœci luminancji
+        for (int pix = 0; pix < rgb.length; pix += 3)
+        {           
+           final double y = RGB2XYZ[3] * rgb[pix] + RGB2XYZ[4] * rgb[pix + 1] + RGB2XYZ[5] * rgb[pix + 2];
+           avg += Math.log(y + 1e-4);
+           max = (y > max) ? y : max ;
+        }
+        avgLuminance = Math.exp(avg / size);
+       
+        // Normalize
+        maxLuminance = max / avgLuminance;
+       
+        // Set divider
+        divider = Math.log10(maxLuminance + 1.0);
 
-        System.out.println("Image width " + hdrImage.getWidth() + ", height " + hdrImage.getHeight());
+        final double biasP = Math.log(1) / -0.693147;
 
-        int i, j;
-        for (int y = 0; y + 3 < hdrImage.getHeight(); y += 3) {
-            for (int x = 0; x + 3 < hdrImage.getWidth(); x += 3) {
-                //System.out.println(x);
-                double average = 0.0;
-                for (i = 0; i < 3; i++) {
-                    for (j = 0; j < 3; j++) {
-                        //System.out.println(x + " " + (x + i) + " " + (y + j));
-                        average += pixels2D[x + i][y + j] / avLum; //(*Y)(x+i,y+j) / avLum;
-                    }
-                }
-                average = average / 9.0 - pixels2D[x][y]; //(*Y)(x,y);
-                if (average > -1.0f && average < 1.0) {
-                    double interpol = Math.log(2.0 + biasFunc(biasP, pixels2D[x + 1][y + 1] / maxLum) * 8.0);
-                    for (i = 0; i < 3; i++) {
-                        for (j = 0; j < 3; j++) {
-                            double Yw = pixels2D[x + i][y + j]; //(*Y)(x+i,y+j);
-                            if (Yw < 1.0f) {
-                                double L = Yw * (6.0 + Yw) / (6.0 + 4.0 * Yw);
-                                Yw = (L / interpol) / divider;
-                            } else if (Yw >= 1.0f && Yw < 2.0f) {
-                                double L = Yw * (6.0 + 0.7662 * Yw) / (5.9897 + 3.7658 * Yw);
-                                Yw = (L / interpol) / divider;
-                            } else {
-                                Yw = (Math.log(Yw + 1.0) / interpol) / divider;
-                            }
+        return scaleLum(biasP, maxLuminance, avgLuminance, divider, rgb, hdrImage.getWidth(), hdrImage.getHeight());
+    }
 
-                            out[x + i][y + j] = Yw;
-                        //(*L)(x+i,y+j) = Yw;
-                        }
-                    }
-                } else {
-                    for (i = 0; i < 3; i++) {
-                        for (j = 0; j < 3; j++) {
-                            double Yw = pixels2D[x + i][y + j];//(*Y)(x+i,y+j);
-                            double interpol = Math.log(2.0f + biasFunc(biasP, Yw / maxLum) * 8.0);
-                            // (*L)(x+i,y+j)
-                            out[x + i][y + j] = (Math.log(Yw + 1.0) / interpol) / divider;
-                        }
-                    }
-                }
+    /**
+     * skaluje wartosci luminancji wg odpowiedniego wspolczynnika (Drago)
+     * 
+     * @param scale
+     *            wspolczynnik skali
+     * @param rgb
+     *            tablica wartoœci obrazu hdr w RGB
+     * @param height
+     *            wysokosc obrazy
+     * @param width
+     *            szerokosc obrazu
+     * @return Image obraz po przeskalowaniu i konwersji do RGB
+     */
+    private static final Image scaleLum(final double biasP, double maxLuminance, double avgLuminance, double divider,
+    		final float[] rgb, int width, int height)
+    {
+    	double luminanceAvgRatio = 0;
+    	double newLum = 0;
+
+        // pusty obraz wynikowy o odpowiednim rozmiarze
+        BufferedImage retImg = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+
+        // konwersja do Yxy i skalowanie
+        for (int pix = 0; pix < rgb.length; pix += 3)
+        {
+            double Y = RGB2XYZ[3] * rgb[pix] + RGB2XYZ[4] * rgb[pix + 1] + RGB2XYZ[5] * rgb[pix + 2];
+            if (Y > 0)
+            {
+                double x = RGB2XYZ[0] * rgb[pix] + RGB2XYZ[1] * rgb[pix + 1] + RGB2XYZ[2] * rgb[pix + 2];
+                double z = RGB2XYZ[6] * rgb[pix] + RGB2XYZ[7] * rgb[pix + 1] + RGB2XYZ[8] * rgb[pix + 2];
+
+                luminanceAvgRatio = Y / avgLuminance;
+                newLum = (Math.log(luminanceAvgRatio + 1.0) / Math.log(2.0 + Math.pow(luminanceAvgRatio / maxLuminance, biasP) * 8.0)) / divider;
+                       
+                // Re-scale to new luminance
+                double scale = newLum / Y;
+                Y *= scale;
+                x *= scale;
+                z *= scale;
+
+                // konwersja z powrotem do RGB
+                final float r = (float) (XYZ2RGB[0] * x + XYZ2RGB[1] * Y + XYZ2RGB[2] * z);
+                final float g = (float) (XYZ2RGB[3] * x + XYZ2RGB[4] * Y + XYZ2RGB[5] * z);
+                final float b = (float) (XYZ2RGB[6] * x + XYZ2RGB[7] * Y + XYZ2RGB[8] * z);
+
+                int rgbValue = 0;
+
+                rgbValue |= Math.min(255, Math.max(0, (int) (r * 255))) << 16;
+                rgbValue |= Math.min(255, Math.max(0, (int) (g * 255))) << 8;
+                rgbValue |= Math.min(255, Math.max(0, (int) (b * 255)));
+
+                // zapisanie do obrazu wynikowego
+                retImg.setRGB((pix / 3) % width, (pix / 3) / width, rgbValue);
+            }
+            else
+            {
+                retImg.setRGB((pix / 3) % width, (pix / 3) / width, 0);
             }
         }
-
-        for (int x = 0; x < hdrImage.getWidth(); x++) {
-            for (int y = 0; y < hdrImage.getHeight(); y++) {
-                double scale = out[x][y] / pixels2D[x][y];
-                pixels2D[x][y] *= scale;
-            }
-        }
-        
-        BufferedImage retImg = new BufferedImage(hdrImage.getWidth(), hdrImage.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-        WritableRaster raster = retImg.getRaster();
-        raster.setPixels(raster.getMinX(), raster.getMinY(), raster.getWidth(), raster.getHeight(), ArrayTools.convert2D1D(pixels2D, hdrImage.getWidth(), hdrImage.getHeight()));
 
         return retImg;
     }
